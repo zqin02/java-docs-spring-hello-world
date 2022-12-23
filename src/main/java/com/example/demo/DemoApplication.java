ec7bc110459b15b9206c8a2435ad018d29a4f8e1
zhaoqin_cheah <qin02qin@gmail.com>
2022-12-23 14:12:55 +0800

    add file path

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -20,5 +26,25 @@ public class DemoApplication {
 
 		logger.info("test test test 123");
 		return "Hello World!";
+	}
+
+
+	@RequestMapping("/file")
+	String createFiles(@RequestBody(required = false) Map<String,String> mapBody) {
+		String location = "/logs/"+System.currentTimeMillis()+".txt";
+		if(mapBody!=null) {
+			 location = mapBody.get("filepath");
+		}
+		try {
+			FileWriter myWriter = new FileWriter(location);
+			myWriter.write("Ftest file share");
+			myWriter.close();
+			logger.info("Successfully wrote to the file.");
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		logger.info("test test test 123 create file");
+		return "File created!";
 	}
 }
